01dc175ac26c0388acc63029b4e4391a045322fb
matheustristao <matheusdabahia1@hotmail.com>
2020-05-29 15:47:23 -0300

    busca lista de lojas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,16 +7,24 @@ class Main extends React.Component {
         super(props);
         this.state = {
             inputproduto: '',
+            inputLoja: '',
             arrayProdutos: [],
+            arrayLojas: [],
             showResults: false,
+            showResultsLoja: false,
             showAlert: false,
             mensagemErro: ''
         };
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeLoja = this.handleChangeLoja.bind(this);
     }
     handleChange(event) {
         this.setState({ inputproduto: event.target.value });
+    }
+
+    handleChangeLoja(event) {
+        this.setState({ inputLoja: event.target.value });
     }
 
     dePara(param) {
@@ -44,6 +52,7 @@ class Main extends React.Component {
             this.setState({
                 showAlert: true,
                 showResults: false,
+                showResultsLoja: false,
                 mensagemErro: 'Nome do produto não pode ser vazio'
             });
         }
@@ -64,13 +73,58 @@ class Main extends React.Component {
                     this.setState({
                         arrayProdutos: response.data,
                         showResults: true,
-                        showAlert: false,
+                        showAlert: false
                     });
                     console.log(this.state.arrayProdutos);
                 }
             } else {
                 this.setState({
-                    showResults: true
+                    showResults: true,
+                    showResultsLoja: false
+                });
+                console.log(response.statusText);
+            }
+        }
+    }
+
+    procuraLoja = async () => {
+
+        if (this.state.inputLoja.trim() === '') {
+            this.setState({
+                showAlert: true,
+                showResults: false,
+                showResultsLoja: false,
+                mensagemErro: 'Nome da loja não pode ser vazio'
+            });
+        }
+        else {
+
+            const response = await api.get("/listalojas?nomeLoja=" + this.state.inputLoja);
+
+            if (response.status === 202) {
+                if (response.data.length === 0) {
+                    console.log(response.data);
+                    this.setState({
+                        arrayLojas: response.data,
+                        showResults: false,
+                        showResultsLoja: false,
+                        showAlert: true,
+                        mensagemErro: "Nenhuma loja encontrado"
+                    });
+                }
+                else {
+                    this.setState({
+                        arrayLojas: response.data,
+                        showResults: false,
+                        showResultsLoja: true,
+                        showAlert: false,
+                    });
+                    console.log(this.state.arrayLojas);
+                }
+            } else {
+                this.setState({
+                    showResults: false,
+                    showResultsLoja: false
                 });
                 console.log(response.statusText);
             }
@@ -80,16 +134,36 @@ class Main extends React.Component {
     render() {
         return (
             <div className="">
-                <header className="jumbotron text-center">
-                    <h1 className="display-4">Cadê meu produto natureba?</h1>
-                    <h5 className="my-4">A gente encontra pra você</h5>
-                    <div className="form-inline justify-content-center">
-                        <form className="form-inline">
-                            <div className="form-group">
-                                <input id="inputProduto" className="form-control mr-sm-2" size="40" type="text" value={this.state.inputproduto} onChange={this.handleChange} placeholder="Ex: PASTA DE AMENDOÍM" />
+
+                <header className="masthead text-white text-center">
+                    <div className="overlay"></div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-9 mx-auto">
+                                <h1 className="mb-5">Cadê meu produto natureba?</h1>
                             </div>
-                            <button id="btnPesquisarProduto" onClick={this.procuraProduto} type="button" className="btn btn-success mr-sm-2">Pesquisar </button>
-                        </form>
+                            <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                                <form>
+                                    <div className="form-row">
+                                        <div className="col-12 col-md-9 mb-2 mb-md-0">
+                                            <input type="text" className="form-control form-control-lg" value={this.state.inputproduto} onChange={this.handleChange} placeholder="Pesquisar por produto"></input>
+                                        </div>
+                                        <div className="col-12 col-md-3">
+                                            <button id="btnPesquisarProduto" onClick={this.procuraProduto} type="button" className="btn btn-block btn-lg btn-primary">Encontrar</button>
+                                        </div>
+                                    </div>
+                                    <h5>Ou</h5>
+                                    <div className="form-row">
+                                        <div className="col-12 col-md-9 mb-2 mb-md-0">
+                                            <input type="text" className="form-control form-control-lg" value={this.state.inputLoja} onChange={this.handleChangeLoja} placeholder="Pesquisar por loja"></input>
+                                        </div>
+                                        <div className="col-12 col-md-3">
+                                            <button id="btnPesquisarLoja" onClick={this.procuraLoja} type="button" className="btn btn-block btn-lg btn-primary">Encontrar</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </header>
 
@@ -122,6 +196,34 @@ class Main extends React.Component {
                         </div>
                     </div>
                 }
+
+                {
+                    this.state.showResultsLoja &&
+                    <div id="showResultsLojaDiv" className="container-fluid text-center">
+                        <div className="row justify-content-center">
+                            <h2>Resultado da busca</h2>
+                        </div>
+                        <div id="resultList" className="row justify-content-center">
+                            <div className="card-deck">
+                                {
+                                    this.state.arrayLojas.map((d, idx) => {
+                                        return (
+                                            <div key={idx} className="card">
+                                                <div className="card-body">
+                                                    <h5 className="card-title">{d.nomeLoja}</h5>
+                                                    <Link to={`/loja/${d.idLoja}`}>
+                                                        <button type="button" className="btn btn-success card-link">Acessar catálogo</button>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                    </div>
+                }
+
 
                 {this.state.showAlert &&
                     <div className="container">
--- src/pages/Product/index.js
+++ src/pages/Product/index.js
@@ -8,13 +8,9 @@ export default class Product extends Component {
         super(props);
         this.state = {
             inputproduto: '',
-            regio: '',
             produto: '',
             arrayProdutos: [],
             arrayLojas: [],
-            showDetail: false,
-            showAlert: false,
-            showInputRegio: false,
             mensagemErro: ''
         };
     }
@@ -27,18 +23,31 @@ export default class Product extends Component {
         if (responseProduto.status === 202 && responseProduto) {
             console.log(responseProduto);
             this.setState({
-                produto: responseProduto.data,
-                showAlert: false,
-                showDetail: false,
-                showInputRegio: true
+                produto: responseProduto.data
             });
+
+            const responseLoja = await api.get("/lojas?idProduto=" + this.state.produto.idProduto);
+
+            if (responseLoja.status === 202) {
+                console.log(responseLoja.data);
+                if (responseLoja.data.length === 0) {
+                    this.setState({
+                        showAlert: true,
+                        mensagemErro: "Nenhuma loja encontrada possui esse produto"
+                    });
+                } else {
+                    this.setState({
+                        arrayLojas: responseLoja.data
+                    });
+                }
+            } else {
+                console.log(responseLoja.statusText);
+            }
+
+
         } else {
             console.log(responseProduto.statusText);
         }
-    }
-
-    handleChangeRegio = (event) => {
-        this.setState({ regio: event.target.value });
     }
 
     deParaGlutenLac(param) {
@@ -72,39 +81,6 @@ export default class Product extends Component {
         return imagem;
     }
 
-    pesquisaProdutoDetail = async () => {
-        if (this.state.regio === '') {
-            this.setState({
-                showAlert: true,
-                showDetail: false,
-                mensagemErro: 'Você deve escolher um estado antes!'
-            });
-        } else {
-
-            const responseLoja = await api.get("/lojas?idProduto=" + this.state.produto.idProduto + "&regio=" + this.state.regio);
-
-            if (responseLoja.status === 202) {
-                console.log(responseLoja.data);
-                if (responseLoja.data.length === 0) {
-                    this.setState({
-                        showAlert: true,
-                        mensagemErro: "Nenhuma loja encontrada no seu estado possui esse produto"
-                    });
-                } else {
-                    this.setState({
-                        arrayLojas: responseLoja.data,
-                        showAlert: false,
-                        showDetail: true,
-                        showInputRegio: false
-                    });
-                }
-            } else {
-                console.log(responseLoja.statusText);
-            }
-
-        }
-    };
-
     render() {
         return (
             <div className="">
@@ -112,129 +88,82 @@ export default class Product extends Component {
                     <h1 className="display-4">Cadê meu produto natureba?</h1>
                     <Link to="/" id="btnPesquisarProduto" type="button" className="btn btn-light">Refazer busca</Link>
                 </header>
-                {this.state.showAlert &&
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="alert alert-danger">
-                                <strong>{this.state.mensagemErro}</strong>
+
+                <div id="showDetail">
+                    <div id="showDetailHeaderDiv" className="container mb-4">
+                        <div id="resultDetailTitle" className="row justify-content-center">
+                            <h3>Descrição do produto</h3>
+                        </div>
+                        <div id="resultDetailHeader" className="row justify-content-center">
+                            <div className="col-sm-3">
+                                <img className="img-thumbnail" src={this.loadImage(this.state.produto.idProduto)} alt={this.state.produto.nomeProduto}></img>
+                            </div>
+                            <div className="col-sm-3">
+                                <dl>
+                                    <dt>Nome</dt>
+                                    <dd>{this.state.produto.nomeProduto}</dd>
+                                    <dt>Marca</dt>
+                                    <dd>{this.dePara(this.state.produto.marcaProduto)}</dd>
+                                    <dt>Descrição</dt>
+                                    <dd>{this.dePara(this.state.produto.descricaoProduto)}</dd>
+                                </dl>
+                            </div>
+                            <div className="col-sm-3">
+                                <dl>
+                                    <dt>Calorias</dt>
+                                    <dd>{this.dePara(this.state.produto.calorias)}</dd>
+                                    <dt>Gluten Free?</dt>
+                                    <dd>{this.deParaGlutenLac(this.state.produto.glutenFree)}</dd>
+                                    <dt>Zero lactose?</dt>
+                                    <dd>{this.deParaGlutenLac(this.state.produto.lacFree)}</dd>
+                                </dl>
                             </div>
                         </div>
                     </div>
-                }
-                {this.state.showInputRegio &&
-                    <div id="showInputRegio">
-                        <div className="container mb-4">
-                            <div className="row justify-content-center">
-                                <h4>Em qual estado você mora?</h4>
-                            </div>
-                            <div className="row justify-content-center">
-                                <div className="col-sm-4 input-group">
-                                    <select className="form-control" value={this.state.regio} onChange={this.handleChangeRegio}>
-                                        <option value=''>Estado</option>
-                                        <option value="SP">São Paulo</option>
-                                        <option value="DF">Distrito Federal</option>
-                                    </select>
-                                    <button type="button" onClick={this.pesquisaProdutoDetail} className="btn btn-success">Encontrar!</button>
-                                </div>
-                            </div>
+                    <div id="showDetailDiv" className="container">
+                        <div id="resultDetailTitle" className="row justify-content-center">
+                            <h3>Lojas encontradas</h3>
                         </div>
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <h5>Produto escolhido</h5>
-                            </div>
-                            <div id="resultDetailHeader" className="row justify-content-center">
-                                <div className="col-sm-3">
-                                    <img className="img-thumbnail" src={this.loadImage(this.state.produto.idProduto)} alt={this.state.produto.nomeProduto}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <dl>
-                                        <dd><strong>Nome: </strong>{this.state.produto.nomeProduto}</dd>
-                                        <dd><strong>Marca: </strong>{this.dePara(this.state.produto.marcaProduto)}</dd>
-                                        <dd> <strong>Descrição: </strong>{this.dePara(this.state.produto.descricaoProduto)}</dd>
-                                    </dl>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                }
-                {this.state.showDetail &&
-                    <div id="showDetail">
-                        <div id="showDetailHeaderDiv" className="container mb-4">
-                            <div id="resultDetailTitle" className="row justify-content-center">
-                                <h3>Descrição do produto</h3>
-                            </div>
-                            <div id="resultDetailHeader" className="row justify-content-center">
-                                <div className="col-sm-3">
-                                    <img className="img-thumbnail" src={this.loadImage(this.state.produto.idProduto)} alt={this.state.produto.nomeProduto}></img>
-                                </div>
-                                <div className="col-sm-3">
-                                    <dl>
-                                        <dt>Nome</dt>
-                                        <dd>{this.state.produto.nomeProduto}</dd>
-                                        <dt>Marca</dt>
-                                        <dd>{this.dePara(this.state.produto.marcaProduto)}</dd>
-                                        <dt>Descrição</dt>
-                                        <dd>{this.dePara(this.state.produto.descricaoProduto)}</dd>
-                                    </dl>
-                                </div>
-                                <div className="col-sm-3">
-                                    <dl>
-                                        <dt>Calorias</dt>
-                                        <dd>{this.dePara(this.state.produto.calorias)}</dd>
-                                        <dt>Gluten Free?</dt>
-                                        <dd>{this.deParaGlutenLac(this.state.produto.glutenFree)}</dd>
-                                        <dt>Zero lactose?</dt>
-                                        <dd>{this.deParaGlutenLac(this.state.produto.lacFree)}</dd>
-                                    </dl>
-                                </div>
-                            </div>
-                        </div>
-                        <div id="showDetailDiv" className="container">
-                            <div id="resultDetailTitle" className="row justify-content-center">
-                                <h3>Lojas encontradas</h3>
-                            </div>
-                            <div id="resultDetail" className="row justify-content-center">
-                                <div className="card-deck">
-                                    {this.state.arrayLojas.map((loja, idx) => {
-                                        return (
-                                            <div key={idx} className="card detailCards">
-                                                <div id="cardStoreHeader" className="card-header text-center"><h4>{loja.nomeLoja}</h4></div>
-                                                <div id={loja.idLoja} className="card-body">
-                                                    <h5 className="card-title">Endereços</h5>
-                                                    {
-                                                        loja.endereco.map((enderecoLoja, idx) => {
-                                                            if (enderecoLoja.regio === this.state.regio) {
-                                                                return (
-                                                                    <ul className="list-group" key={idx}>
-                                                                        <li className="list-group-item btnProdutoDetail" >
-                                                                            <p><span>Endereço:</span>  {enderecoLoja.local}</p>
-                                                                            <p><span>Telefone:</span>  {enderecoLoja.telefone}</p>
-                                                                        </li>
-                                                                    </ul>
-                                                                )
-                                                            }
-                                                        })
-                                                    }
-                                                    <h5 className="card-title">Redes Sociais</h5>
-                                                    <p className="card-text text-center">
-                                                        <a className="mr-sm-2" href={loja.enderecosVirtuais.facebook} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/facebook.png')} alt="facebook"></img> </a>
-                                                        <a className="mr-sm-2" href={loja.enderecosVirtuais.instagram} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/instagram.png')} alt="instagram"></img> </a>
-                                                        <a className="mr-sm-2" href={loja.enderecosVirtuais.website} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/browser.png')} alt="website"></img></a>
-                                                    </p>
-                                                    <div className="text-center">
-                                                        <Link to={`/loja/${loja.idLoja}`}>
-                                                            <button className="btn btn-success">Acessar catálogo da loja</button>
-                                                        </Link>
-                                                    </div>
+                        <div id="resultDetail" className="row justify-content-center">
+                            <div className="card-deck">
+                                {this.state.arrayLojas.map((loja, idx) => {
+                                    return (
+                                        <div key={idx} className="card detailCards">
+                                            <div id="cardStoreHeader" className="card-header text-center"><h4>{loja.nomeLoja}</h4></div>
+                                            <div id={loja.idLoja} className="card-body">
+                                                <h5 className="card-title">Endereços</h5>
+                                                {
+                                                    loja.endereco.map((enderecoLoja, idx) => {
+                                                        return (
+                                                            <ul className="list-group" key={idx}>
+                                                                <li className="list-group-item btnProdutoDetail" >
+                                                                    <p><span>Endereço:</span>  {enderecoLoja.local}</p>
+                                                                    <p><span>Telefone:</span>  {enderecoLoja.telefone}</p>
+                                                                </li>
+                                                            </ul>
+                                                        )
+                                                    })
+                                                }
+                                                <h5 className="card-title">Redes Sociais</h5>
+                                                <p className="card-text text-center">
+                                                    <a className="mr-sm-2" href={loja.enderecosVirtuais.facebook} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/facebook.png')} alt="facebook"></img> </a>
+                                                    <a className="mr-sm-2" href={loja.enderecosVirtuais.instagram} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/instagram.png')} alt="instagram"></img> </a>
+                                                    <a className="mr-sm-2" href={loja.enderecosVirtuais.website} target="_blank" rel="noopener noreferrer"> <img className="i" src={require('../images/icons/browser.png')} alt="website"></img></a>
+                                                </p>
+                                                <div className="text-center">
+                                                    <Link to={`/loja/${loja.idLoja}`}>
+                                                        <button className="btn btn-success">Acessar catálogo da loja</button>
+                                                    </Link>
                                                 </div>
                                             </div>
-                                        )
-                                    })}
-                                </div>
+                                        </div>
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
-                }
+                </div>
+
             </div>
         );
     }
